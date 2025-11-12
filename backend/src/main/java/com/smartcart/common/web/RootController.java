47c1ab90ed50faa51c7c91e3beed1ec07e5d091a
Ruchitha Nandikonda <ruchithanandikonda@Ruchithas-Laptop-61.local>
2025-11-12 15:56:34 -0700

    Add RootController for / route

---
+++ backend/src/main/java/com/smartcart/common/web/RootController.java
@@ -0,0 +1,12 @@
+package com.smartcart.common.web;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("SmartCart backend is up");
+    }
+}
